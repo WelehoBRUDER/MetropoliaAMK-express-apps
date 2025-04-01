bef01a5c2fc3f4ad5fb9312870ca8eb9fe7f60ee
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-04-01 15:11:56 +0300

    error handling

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from "express";
 import cors from "cors";
+import {notFoundHandler, errorHandler} from "./middlewares.js";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use("/public", express.static("public"));
+
+// Default for all routes not handled by routers above
+app.use(notFoundHandler);
+// Add error handler middleware as the last middleware in the chain
+app.use(errorHandler);
 
 export default app;
--- src/middlewares.js
+++ src/middlewares.js
@@ -37,4 +37,22 @@ const authenticateToken = async (req, res, next) => {
   }
 };
 
-export {createThumbnail, authenticateToken};
+const notFoundHandler = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  error.status = 404;
+  next(error); // forward error to error handler
+};
+/**
+ * Custom default middleware for handling errors
+ */
+const errorHandler = (err, req, res, next) => {
+  res.status(err.status || 500); // default is 500 if err.status is not defined
+  res.json({
+    error: {
+      message: err.message,
+      status: err.status || 500,
+    },
+  });
+};
+
+export {createThumbnail, authenticateToken, notFoundHandler, errorHandler};
